15a83007369ace53479fbfa4f6f1459bf9e455d5
Ed Cook <eecook.91@gmail.com>
2016-01-30 20:38:42 +0000

    Adds random number of moves and improvements.

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -8,30 +8,32 @@ public class GameController : MonoBehaviour
 {
     public Material arrowMaterial;
     public Material completedArrowMaterial;
-    public int totalMoves = 5;
-
+    public int minMoves = 3;
+    public int maxMoves = 7;
     public PlayerController playerController;
+    public Text ritualName;
 
-    public Text ritual_name;
+    private int total_moves;
     private List<GameObject> quads;
     private List<int> generated_moves;
     private Transform quad_container;
     private int current_move = 0;
 
     // Use this for initialization
-    void Start() { 
+    void Start()
+    {
         quad_container = new GameObject("Quad Container").transform;
         quads = new List<GameObject>();
         generated_moves = new List<int>();
+        total_moves = Random.Range(minMoves, maxMoves + 1);
+        ritualName.text = RitualNames[Random.Range(0, RitualNames.Count)];
 
-        ritual_name.text = RitualNames[Random.Range(0, RitualNames.Count)];
-
-        for (int i = 0; i < totalMoves; ++i)
+        for (int i = 0; i < total_moves; ++i)
         {
             GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
 
             quad.GetComponent<MeshRenderer>().material = arrowMaterial;
-            quad.transform.position = new Vector3(i - 2.5f, 3, -2);
+            quad.transform.position = new Vector3(i + (1 - total_moves) / 2f, 3, -2);
 
             int move = Random.Range(0, 3);
             Vector3 arrowRotation = ArrowRotationForMove(move);
@@ -46,28 +48,31 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int playerMove = playerController.ReturnMove();
-        if (generated_moves[current_move] == playerMove)
+        if (current_move < total_moves)
         {
-            //change texture
-            quads[current_move].GetComponent<MeshRenderer>().material = completedArrowMaterial;
-
-            // play animation
-
-            // progress
-            ++current_move;
-        }
-        else if (playerMove != -1)
-        {
-            // play animation
-            // reset
-            for(int i = 0; i < current_move; ++i)
+            int playerMove = playerController.ReturnMove();
+            if (generated_moves[current_move] == playerMove)
             {
-                quads[i].GetComponent<MeshRenderer>().material = arrowMaterial;
+                //change texture
+                quads[current_move].GetComponent<MeshRenderer>().material = completedArrowMaterial;
+
+                // play animation
+
+                // progress
+                ++current_move;
             }
-            current_move = 0;
+            else if (playerMove != -1)
+            {
+                // play animation
+                // reset
+                for (int i = 0; i < current_move; ++i)
+                {
+                    quads[i].GetComponent<MeshRenderer>().material = arrowMaterial;
+                }
+                current_move = 0;
+            }
         }
-}
+    }
 
     private Vector3 ArrowRotationForMove(int move)
     {
