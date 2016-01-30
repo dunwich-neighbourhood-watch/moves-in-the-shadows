093962aa52ee408c5dabb013ec82d886727c95aa
John Children <john.a.children@gmail.com>
2016-01-30 12:34:04 +0000

    Adds arrow quads

--- moves-in-the-shadows/Assets/Game/Scripts/GameController.cs
+++ moves-in-the-shadows/Assets/Game/Scripts/GameController.cs
@@ -1,36 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
+
+    public Material arrowMaterial;
+    public Material completedArrowMaterial;
 
     private Timer game_time;
 
     private int total_moves;
-    private ArrayList generated_moves;
-    private ArrayList player_moves;
+    private List<int> generated_moves;
+    private List<GameObject> quads;
 
 	// Use this for initialization
-	void Start () {
-        game_time = new Timer();
+	void Start ()
+    {
+        generated_moves = new List<int>();
+        quads = new List<GameObject>();
         total_moves = 5;
 
         for (int i = 0; i <= total_moves; ++i)
         {
-            generated_moves.Add(Random.Range(0, 3));
+            int move = Random.Range(0, 3);
+            generated_moves.Add(move);
         }
-	}
+
+        // create quads with moves on
+        foreach (int move in generated_moves)
+        {
+            quads.Add(GameObject.CreatePrimitive(PrimitiveType.Quad));
+            
+        }
+        for (int i = 0; i < quads.Count; i++)
+        {
+            quads[i].GetComponent<MeshRenderer>().material = arrowMaterial;
+            quads[i].transform.position = new Vector3(i - 3, 3, -2);
+        }
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	    if (game_time.GetCurrent() <= 10.0f)
+	void Update ()
+    {
+	    if (true)//game_time.GetCurrent() <= 10.0f)
         {
             // wait
         }
         else
         {
-            // create quads with moves on
             // move quads across screen
-
+            // record player input
         }
 	}
 }
