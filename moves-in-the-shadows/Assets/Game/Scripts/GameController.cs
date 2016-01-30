34ab8257aa1e5abbe02fc6719e22a596c07c3e98
John Children <john.a.children@gmail.com>
2016-01-30 10:43:02 +0000

    Adds outline for game logic

---
+++ moves-in-the-shadows/Assets/Game/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private Timer game_time;
+
+    private int total_moves;
+    private ArrayList generated_moves;
+    private ArrayList player_moves;
+
+	// Use this for initialization
+	void Start () {
+        game_time = new Timer();
+        total_moves = 5;
+
+        for (int i = 0; i <= total_moves; ++i)
+        {
+            generated_moves.Add(Random.Range(0, 3));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (game_time.GetCurrent() <= 10.0f)
+        {
+            // wait
+        }
+        else
+        {
+            // create quads with moves on
+            // move quads across screen
+
+        }
+	}
+}
---
+++ moves-in-the-shadows/Assets/Game/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    private float start_time;
+    private float current_time;
+
+	// Use this for initialization
+	void Start ()
+    {
+        start_time = Time.timeSinceLevelLoad;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        current_time = Time.timeSinceLevelLoad;
+    }
+
+    public float GetStart ()
+    {
+        return start_time;
+    }
+
+    public float GetCurrent ()
+    {
+        return current_time;
+    }
+}
